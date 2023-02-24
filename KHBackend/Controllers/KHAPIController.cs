477f23588f0c658bc1ef30623a6cdd3e22a86b32
bodnarbalazs <novroax@gmail.com>
2023-02-25 00:41:40 +0100

    api controller created

---
+++ KHBackend/Controllers/KHAPIController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KHBackend.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class KHAPIController : ControllerBase
+    {
+        [HttpGet]
+        [Route("test")]
+        public IActionResult Test()
+        {
+
+            return Ok("Anyád");
+        }
+    }
+}
