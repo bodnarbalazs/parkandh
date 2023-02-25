392bff73b570828108f1a01119788cf8577f5f56
bodnarbalazs <novroax@gmail.com>
2023-02-25 16:43:00 +0100

    .

--- KHBackend/Controllers/KHAPIController.cs
+++ KHBackend/Controllers/KHAPIController.cs
@@ -9,10 +9,53 @@ namespace KHBackend.Controllers
     {
         [HttpGet]
         [Route("getUserByEmail/{Email}/{Password}")]
-        public IActionResult Test(string email)
+        public IActionResult GetUserByEmail(string email,string password)
         {
              
             return Ok(email);
+        }
+        [HttpGet]
+        [Route("getReservationsByUserId/{UserId}")]
+        public IActionResult GetReservationsByUserId(int UserId)
+        {
+
+            return Ok(UserId);
+        }
+        [HttpGet]
+        [Route("getAvailableCarPools/{UserId}")]
+        public IActionResult GetAvailableCarPools(int UserId)
+        {
+
+            return Ok(UserId);
+        }
+        [HttpPost]
+        [Route("postParkingRequest/{UserId}/{FromDate}/{ToDate}")]
+        public IActionResult PostParkingRequest(int UserId,DateTime fromDate,DateTime toDated)
+        {
+
+            return Ok(UserId);
+        }
+        [HttpPost]
+        [Route("postReservationSubmission/{UserId}/{FromDate}/{ToDate}/{Beneficiary}")]
+        public IActionResult PostReservationSubmission(int UserId, DateTime fromDate, DateTime toDate, int BeneficiaryId)
+        {
+
+            return Ok(UserId);
+        }
+        [HttpPost]
+        [Route("postReservationSubmissionCancellation/{UserId}/{FromDate}/{ToDate}")]
+        public IActionResult PostReservationSubmissionCancellation(int UserId, DateTime fromDate, DateTime toDate, int BeneficiaryId)
+        {
+
+            return Ok(UserId);
+        }
+        [HttpPost]
+        [Route("postUserSettingsUpdate/{UserId}/{UserSetting}")]
+        
+        public IActionResult PostUserSettingsUpdate(int UserId, string userSettings)
+        {
+
+            return Ok(UserId);
         }
     }
 }
