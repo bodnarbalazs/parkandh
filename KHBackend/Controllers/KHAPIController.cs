7bd3b27c1268bad7cbb7d555b3e82ea689551ec6
bodnarbalazs <novroax@gmail.com>
2023-02-25 18:17:24 +0100

    .

--- KHBackend/Controllers/KHAPIController.cs
+++ KHBackend/Controllers/KHAPIController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using KHBackend.Models;
 
 namespace KHBackend.Controllers
 {
@@ -11,8 +12,17 @@ namespace KHBackend.Controllers
         [Route("getUserByEmail/{Email}/{Password}")]
         public IActionResult GetUserByEmail(string email,string password)
         {
-             
-            return Ok(email);
+            ParkContext parkContext = new ParkContext();
+            User? user =parkContext.Users.Where(u => u.Email == email).ToList().FirstOrDefault();
+            if (user==null)
+            {
+                return NotFound("Hibás felhasználónév vagy jelszó.");
+            }
+            else if(password != user.Password)
+            {
+                return NotFound("Hibás felhasználónév vagy jelszó.");
+            }
+            return Ok(user);
         }
         [HttpGet]
         [Route("getReservationsByUserId/{UserId}")]
