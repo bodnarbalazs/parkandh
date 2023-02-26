5c28e52b8ac4ed8b45d34c7aa30d2307b1fd62bb
VSZA <vszandras@gmail.com>
2023-02-26 06:49:48 +0100

    asdasd

--- KHBackend/wwwroot/test.js
+++ KHBackend/wwwroot/test.js
@@ -58,6 +58,9 @@ function calendarId(){
       ];
     document.querySelectorAll(".da").forEach(function(item){
         const day=item.innerText;
+        if (0<day && day<10) {
+            day= "0"+day  
+        }
         item.id=year+"-"+"0"+(monthNames.indexOf(month)+1)+"-"+day;
     })
 }
