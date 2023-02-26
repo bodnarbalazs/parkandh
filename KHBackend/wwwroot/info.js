f280defc2cb4cf3408b1607047634b7725ecd572
VSZA <vszandras@gmail.com>
2023-02-26 04:43:57 +0100

    swipe test 1

--- KHBackend/wwwroot/info.js
+++ KHBackend/wwwroot/info.js
@@ -7,3 +7,51 @@ function info() {
     }
     console.log("work");
 }
+
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+                                                                         
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+                                                                         
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+    console.log("x: "+xDiff);
+    console.log("y: "+yDiff);
+    // Get the screen width
+    var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+    // Check if the swipe is bigger than half of the screen width
+    if ( Math.abs( xDiff ) > 12 ) {
+        if ( xDiff > 0) {
+            location.replace("");
+            alert("right");
+            console.log(xDiff)
+        } else {
+            alert("left");
+        }                       
+    }
+
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
