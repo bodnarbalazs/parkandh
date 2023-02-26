9c4bfa18508bdf04cbef5696bd1b2dec2155bbca
bodnarbalazs <novroax@gmail.com>
2023-02-26 08:12:06 +0100

    .

---
+++ KHBackend/wwwroot/atad2.js
@@ -0,0 +1,9 @@
+﻿function SubmitParkingReservation() {
+    const resId = "";
+    const uId = "";
+    fetch("postReservationSubmissionWithId/" + resId + "/" + uId)
+        .then(r => JSON.parse(r))
+        .then(d => {
+
+        });
+}
