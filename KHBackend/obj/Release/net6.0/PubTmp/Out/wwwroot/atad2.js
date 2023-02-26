a56cd60f2d4123014ef77a5b5157a740ae37f495
bodnarbalazs <novroax@gmail.com>
2023-02-26 08:41:21 +0100

    .

--- KHBackend/obj/Release/net6.0/PubTmp/Out/wwwroot/atad2.js
+++ KHBackend/obj/Release/net6.0/PubTmp/Out/wwwroot/atad2.js
@@ -7,3 +7,18 @@
 
         });
 }
+
+function giveSpot(element){
+    var clickedId = element.target.id;
+    console.log(clickedId);
+    fetch("api/getReservationsByUserId/"+user.id)
+    .then((response) => response.json())
+        .then((data) => {
+            for (let i = 0; i < data.length; i++) {
+                if (data[i].fromDate.split('T')[0]=clickedId) {
+                    console.log(data[i].id)
+                }
+                
+            }
+        })
+}
