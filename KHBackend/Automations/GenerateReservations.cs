9cafcd5086507eae22d28f3bb6d662aeab6f2f0e
bodnarbalazs <novroax@gmail.com>
2023-02-26 02:30:21 +0100

    .

---
+++ KHBackend/Automations/GenerateReservations.cs
@@ -0,0 +1,28 @@
+﻿using KHBackend.Models;
+namespace KHBackend.Automations
+{
+    public class GenerateReservations
+    {
+        public void Setup()
+        {
+            DateTime currentDate = DateTime.Now;
+            ParkContext parkContext = new ParkContext();
+            int dayLimit = 30;
+
+            parkContext.Users.Where(u => u.PrivateParking != "null").ToList().ForEach(user =>
+               {
+                   for (int i = 0; i < dayLimit; i++)
+                   {
+                       
+                   }
+               });
+        }
+            public void Daily()
+        {
+            DateTime currentDate=DateTime.Now;
+            ParkContext parkContext = new ParkContext();
+
+            parkContext.Users.ToList().ForEach(user =>parkContext.ParkingReservations.Add(new )
+        }
+    }
+}
